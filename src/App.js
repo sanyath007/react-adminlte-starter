0f9e1d21a8cce249599ee7ab3454bf26d0635bfc
sanyath007 <sanyath007@gmail.com>
2021-04-14 10:57:44 +0700

    Create new view Register page and add it to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
 import MainLayout from './layouts/MainLayout';
 import Login from './views/Login';
+import Register from './views/Register';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+        <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
         <Route path="/" name="Home" render={props => <MainLayout {...props} />} />
       </Switch>
     </BrowserRouter>
